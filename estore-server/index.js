0f278e496a325a35bbb68890f7d8a47b1c36709e
Maruf520 <md.maruf5201@gmail.com>
2024-12-24 11:40:27 +0600

    product category route added

--- estore-server/index.js
+++ estore-server/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
+const productCategories = require('./routes/productCategories');
 const app = express();
 const PORT = 5001;
 
-app.get('/', (req, res) => {
-  let prodData = {
-    pName: 'Jackets',
-    price: 45,
-    img: 'shop-1.jpg',
-  };
-  res.status(200).send(prodData);
-});
+app.use('/productCategories', productCategories);
 
 const server = app.listen(PORT, () => {
   console.log('App is running on the port - 5001');
