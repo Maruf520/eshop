b33c3adf6b96cdf4201d5fbeb720ed32cca6a0a2
Maruf520 <md.maruf5201@gmail.com>
2024-12-28 11:17:36 +0600

    form validation addedf

--- estore/src/app/home/components/users/user-signup/user-signup.component.ts
+++ estore/src/app/home/components/users/user-signup/user-signup.component.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  AbstractControl,
+  Validator,
+  Validators,
+} from '@angular/forms';
+import { matchPasswords } from './validators/match-password.validator';
 
 @Component({
   selector: 'app-user-signup',
@@ -14,17 +22,37 @@ export class UserSignupComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
-    this.userSignupForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      address: [''],
-      city: [''],
-      state: [''],
-      pin: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-    });
+    this.userSignupForm = this.fb.group(
+      {
+        firstName: ['', [Validators.required]],
+        lastName: [''],
+        address: [''],
+        city: [''],
+        state: [''],
+        pin: [''],
+        email: ['', [Validators.required], [Validators.email]],
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        Validator: matchPasswords,
+      }
+    );
   }
+
+  get firstName(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('firstName');
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('email');
+  }
+  get password(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('password');
+  }
+  get confirmPassword(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('confirmPassword');
+  }
+
   onSubmit(): void {}
 }
