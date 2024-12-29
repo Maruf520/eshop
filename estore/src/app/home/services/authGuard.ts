53b2867b0b1f381e43e1b63950139722137f31d6
Maruf520 <md.maruf5201@gmail.com>
2024-12-29 17:22:27 +0600

    simple design added to the not found page

--- estore/src/app/home/services/authGuard.ts
+++ estore/src/app/home/services/authGuard.ts
@@ -1,4 +1,4 @@
-import { inject, Inject } from '@angular/core';
+import { inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   createUrlTreeFromSnapshot,
@@ -7,7 +7,7 @@ import { map } from 'rxjs';
 import { UserService } from './users/user-service.service';
 
 export const authGuard = (next: ActivatedRouteSnapshot) => {
-  inject(UserService).isUserAuthenticated$.pipe(
+  return inject(UserService).isUserAuthenticated$.pipe(
     map((isAuthenticated) =>
       isAuthenticated
         ? true
